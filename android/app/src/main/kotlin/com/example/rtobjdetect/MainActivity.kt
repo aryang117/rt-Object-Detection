1feba6f4d934b2b78e50b37ba0f5873987fd5d6a
Trolled00Gamer <aryu100@gmail.com>
2021-05-22 11:01:55 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/rtobjdetect/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rtobjdetect
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+//@dart 2.9
+
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'home.dart';
+
+List<CameraDescription> cameras;
+
+Future<Null> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    print('Error: $e.code\nError Message: $e.message');
+  }
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'RealTime Detection',
+      home: HomePage(cameras),
+    );
+  }
+}
