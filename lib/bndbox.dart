5ba0e14c4f6ff30af8045254a62bc026e7a75050
Trolled00Gamer <aryu100@gmail.com>
2021-05-25 23:19:40 +0530

    added tts to detected objects

--- lib/bndbox.dart
+++ lib/bndbox.dart
@@ -1,8 +1,9 @@
 //@dart = 2.9
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
+import 'package:flutter_tts/flutter_tts.dart';
 
-class BndBox extends StatelessWidget {
+class BndBox extends StatefulWidget {
   final List<dynamic> results;
   final int previewH;
   final int previewW;
@@ -18,28 +19,44 @@ class BndBox extends StatelessWidget {
   );
 
   @override
+  _BndBoxState createState() => _BndBoxState();
+}
+
+class _BndBoxState extends State<BndBox> {
+  @override
   Widget build(BuildContext context) {
+    FlutterTts flutterTts = new FlutterTts();
+
+    Future _speak(String object) async {
+      var result = await flutterTts.speak(object);
+    }
+
     List<Widget> _renderBox() {
-      return results.map((re) {
+      return widget.results.map((re) {
         var _x = re["rect"]["x"];
         var _w = re["rect"]["w"];
         var _y = re["rect"]["y"];
         var _h = re["rect"]["h"];
         var scaleW, scaleH, x, y, w, h;
 
-        if (screenH / screenW > previewH / previewW) {
-          scaleW = screenH / previewH * previewW;
-          scaleH = screenH;
-          var difW = (scaleW - screenW) / scaleW;
+        print("the results are as follows " + re["detectedClass"].toString());
+
+        _speak(re["detectedClass"].toString());
+
+        if (widget.screenH / widget.screenW >
+            widget.previewH / widget.previewW) {
+          scaleW = widget.screenH / widget.previewH * widget.previewW;
+          scaleH = widget.screenH;
+          var difW = (scaleW - widget.screenW) / scaleW;
           x = (_x - difW / 2) * scaleW;
           w = _w * scaleW;
           if (_x < difW / 2) w -= (difW / 2 - _x) * scaleW;
           y = _y * scaleH;
           h = _h * scaleH;
         } else {
-          scaleH = screenW / previewW * previewH;
-          scaleW = screenW;
-          var difH = (scaleH - screenH) / scaleH;
+          scaleH = widget.screenW / widget.previewW * widget.previewH;
+          scaleW = widget.screenW;
+          var difH = (scaleH - widget.screenH) / scaleH;
           x = _x * scaleW;
           w = _w * scaleW;
           y = (_y - difH / 2) * scaleH;
